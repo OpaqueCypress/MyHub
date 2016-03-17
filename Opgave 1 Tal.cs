1add411e8f351b4f0749f3c7e9a2e1e3945db6c9
OpaqueCypress <hansen618@hotmail.com>
2016-03-17 10:56:13 +0100

    Added files via upload

---
+++ Opgave 1 Tal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int x = 1, Y = 4;
+
+        if (x <= 1)
+        {
+            Console.Write(x);
+        }
+
+        else
+        {
+            Console.Write(Y);
+        }
+
+        Console.ReadKey();        
+        
+        
+        }
+    }
+}
---
+++ Opgave 2 find fejlen i programmet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 1, Y = 4;
+
+            if (x <= 1)
+
+            {
+                Console.Write(x);
+            }
+ 
+            else 
+
+            {
+             Console.Write(Y);
+            }
+
+            Console.ReadKey();
+
+
+        
+        
+        
+        
+        }
+    }
+}
